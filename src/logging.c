ad4febd7f943c9f24d6a8763913151e514c2f718
Cor Peters <corpeters@incas3.nl>
2015-01-15 15:52:32 +0100

    fix memory leaks

--- src/logging.c
+++ src/logging.c
@@ -30,6 +30,14 @@ struct logging {
     struct log_entry *log_last;
 };
 
+static void le_free(struct log_entry *e) {
+    for (int i = 0; i < e->atom_lst_sz; i++) {
+        free(e->atom_lst[i].string);
+    }
+    free(e->atom_lst);
+    free(e);
+}
+
 struct logging *lg_init(char *logfile, enum lg_debug_levels lvl, int max_size) {
     struct logging *log_ctx = calloc(1, sizeof(struct logging) );
     if (log_ctx == NULL) return NULL;
@@ -50,6 +58,12 @@ struct logging *lg_init(char *logfile, enum lg_debug_levels lvl, int max_size) {
 void lg_exit(struct logging *log_ctx) {
     if (log_ctx == NULL) return;
     if (log_ctx->log_fd != NULL) free(log_ctx->log_fd);
+
+    while (queue_size(log_ctx->logging_q) > 0) {
+        struct log_entry *tmp = (struct log_entry *) queue_pop_head(log_ctx->logging_q);
+        le_free(tmp);
+    }
+
     queue_exit(log_ctx->logging_q);
     fclose(log_ctx->log_file);
     free(log_ctx);
@@ -87,14 +101,6 @@ static bool le_is_equal(struct log_entry *a, struct log_entry *b) {
     }
 
     return true;
-}
-
-static void le_free(struct log_entry *e) {
-    for (int i = 0; i < e->atom_lst_sz; i++) {
-        free(e->atom_lst[i].string);
-    }
-    free(e->atom_lst);
-    free(e);
 }
 
 static void lg_print_to_file(struct logging *log_ctx, struct log_entry *entry) {
@@ -159,8 +165,6 @@ static void lg_print_to_queue(struct logging *log_ctx, struct log_entry *entry) 
 
 #define STRING_MAX 500
 void lg_printf_basic(struct logging *log_ctx, enum lg_debug_levels dbg_lvl, const char* module, const char* format, va_list args) {
-    return; /* DEBUG */
-
     if ( (log_ctx != NULL) && (dbg_lvl > log_ctx->level) ) return;
     struct log_entry *le = calloc(1, sizeof(struct log_entry) );
     if (le == NULL) return;
@@ -295,7 +299,6 @@ void msg_add(enum msg_fd fd, enum lg_channel c, const char *format, ...) {
     assert(gbl_log != NULL);
     if (gbl_log->log_fds_active[fd] == false) return;
     struct log_entry *le = gbl_log->log_fd;
-    return; /* DEBUG */
 
     le->atom_lst_sz++;
     le->atom_lst = realloc(le->atom_lst, le->atom_lst_sz * sizeof(struct log_atom) );
