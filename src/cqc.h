79b5aa319d60a72747b101eebf55543d15572f85
Cor Peters <corpeters@incas3.nl>
2015-01-19 20:05:25 +0100

    fix cqc

--- src/cqc.h
+++ src/cqc.h
@@ -9,8 +9,8 @@ struct cqc {
 };
 
 #define cqc_init(cqc, sz) do { (cqc).size = sz; (cqc).tail = 0; (cqc).head = 0; } while (0)
-#define cqc_cnt(cqc) ( (cqc).head - (cqc).tail) % ( (cqc).size -1)
-#define cqc_space(cqc) ( (cqc).size - cqc_cnt(cqc) -1)
+#define cqc_cnt(cqc) ( (cqc).head - (cqc).tail)
+#define cqc_space(cqc) ( ( (cqc).size - cqc_cnt(cqc) ) -1)
 #define cqc_put(cqc) ( (cqc).head++ ) % ( (cqc).size -1)
 #define cqc_get(cqc) ( (cqc).tail++ ) % ( (cqc).size -1)
 #define cqc_peek(cqc, idx) ( (cqc).tail + idx ) % ( (cqc).size -1)
