1b742b6eb54422820f7997b590180ff8283cbeeb
Cor Peters <corpeters@incas3.nl>
2015-01-20 14:58:16 +0100

    fixed minor count bugs, added cqc_qsz to request size of the queue

--- src/cqc.h
+++ src/cqc.h
@@ -12,23 +12,25 @@ struct cqc {
 #ifdef CQC_USE_MODULO
 
 #define cqc_init(cqc, sz) do { (cqc).size = sz; (cqc).tail = 0; (cqc).head = 0; } while (0)
-#define cqc_cnt(cqc)        ( ((cqc).head-(cqc).tail) % ((cqc).size-1) )
-#define cqc_put(cqc)        ( ((cqc).head++)          % ((cqc).size-1) )
-#define cqc_get(cqc)        ( ((cqc).tail++)          % ((cqc).size-1) )
-#define cqc_peek(cqc, idx)  ( ((cqc).tail+idx)        % ((cqc).size-1) )
-#define cqc_space(cqc)      ( ((cqc).size-1)          - (cqc_cnt(cqc)) )
+#define cqc_qsz(cqc)        ( (cqc).size )
+#define cqc_cnt(cqc)        ( ((cqc).head-(cqc).tail) % ((cqc).size) )
+#define cqc_put(cqc)        ( ((cqc).head++)          % ((cqc).size) )
+#define cqc_get(cqc)        ( ((cqc).tail++)          % ((cqc).size) )
+#define cqc_peek(cqc, idx)  ( ((cqc).tail+idx)        % ((cqc).size) )
+#define cqc_space(cqc)      ( ((cqc).size)            - (cqc_cnt(cqc)) )
 
 #elif !defined(CQC_USE_MODULO)
 
 /* round to the power of two */
-#define cqc_rnd_to_pwr2(sz) do { sz--; sz |= sz>>1; sz |= sz>>2; sz |= sz>>4; sz |= sz>>8; sz |= sz>>16; sz++; } while (0)
+#define cqc_rnd_to_pwr2(sz) do { sz |= sz>>1; sz |= sz>>2; sz |= sz>>4; sz |= sz>>8; sz |= sz>>16; sz++; } while (0)
 
-#define cqc_init(cqc, sz) do { cqc_rnd_to_pwr2(sz); (cqc).size = sz; (cqc).tail = 0; (cqc).head = 0; } while (0)
-#define cqc_cnt(cqc)        ( ((cqc).head-(cqc).tail) & ((cqc).size-1) )
-#define cqc_put(cqc)        ( ((cqc).head++)          & ((cqc).size-1) )
-#define cqc_get(cqc)        ( ((cqc).tail++)          & ((cqc).size-1) )
-#define cqc_peek(cqc, idx)  ( ((cqc).tail+idx)        & ((cqc).size-1) )
-#define cqc_space(cqc)      ( ((cqc).size-1)          - (cqc_cnt(cqc)) )
+#define cqc_init(cqc, sz) do { uint32_t sz2 = (sz); if ( (sz2 & (sz2-1) ) != 0) cqc_rnd_to_pwr2(sz2); (cqc).size = sz2-1; (cqc).tail = 0; (cqc).head = 0; } while (0)
+#define cqc_qsz(cqc)        ( (cqc).size+1 )
+#define cqc_cnt(cqc)        ( ((cqc).head-(cqc).tail) & ((cqc).size) )
+#define cqc_put(cqc)        ( ((cqc).head++)          & ((cqc).size) )
+#define cqc_get(cqc)        ( ((cqc).tail++)          & ((cqc).size) )
+#define cqc_peek(cqc, idx)  ( ((cqc).tail+idx)        & ((cqc).size) )
+#define cqc_space(cqc)      ( ((cqc).size)            - (cqc_cnt(cqc)) )
 
 #endif /* CQC_USE_MODULO */
 
