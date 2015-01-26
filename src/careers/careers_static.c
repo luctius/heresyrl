6f5fba63cf8accd4d4d2c7dfbaf71e56f7a30593
Cor Peters <corpeters@incas3.nl>
2015-01-26 15:19:21 +0100

    added allies

--- src/careers/careers_static.c
+++ src/careers/careers_static.c
@@ -21,6 +21,7 @@
 #define TRAPPINGS(t...) .trappings = { t, IID_NONE, } 
 #define EXITS(e...)   .exit_template_ids = { e, CRID_NONE, } 
 #define DESCR(d)      .description=d
+#define ALLIES(ally_id) .allies_ids={ ally_id, MID_NONE, }
 
 static struct cr_career static_career_list[] = {
     CAREER(CRID_NONE, " "),
@@ -32,6 +33,7 @@ static struct cr_career static_career_list[] = {
         SKILLS(bf(MSR_SKILLS_DODGE) | bf(MSR_SKILLS_HEAL) | bf(MSR_SKILLS_AWARENESS) ),
         TALENTS(TLT_QUICK_DRAW, TLT_SPEC_WPN_GRP_PARRYING, TLT_STREET_FIGHTING, TLT_STRIKE_TO_STUN, TLT_VERY_STRONG, TLT_VERY_RESILIENT),
         TRAPPINGS(IID_THROWING_KNIFE, IID_THROWING_KNIFE, IID_LEATHER_JACK),
+        ALLIES(MID_NONE),
         EXITS(CRID_MERCENARY),
         .weight    = { [MSR_RACE_DWARF]=3,    [MSR_RACE_ELF]=0,    [MSR_RACE_HALFLING]=0,    [MSR_RACE_HUMAN]=200, },
         .available = { [MSR_RACE_DWARF]=true, [MSR_RACE_ELF]=true, [MSR_RACE_HALFLING]=true, [MSR_RACE_HUMAN]=true, },
@@ -43,6 +45,7 @@ static struct cr_career static_career_list[] = {
         SKILLS(bf(MSR_SKILLS_DODGE) | bf(MSR_SKILLS_HEAL) | bf(MSR_SKILLS_AWARENESS) ),
         TALENTS(TLT_QUICK_DRAW, TLT_RAPID_RELOAD, TLT_STRIKE_MIGHTY_BLOW, TLT_SHARPSHOOTER, TLT_STRIKE_TO_STUN),
         TRAPPINGS(IID_NONE),
+        ALLIES(MID_NONE),
         EXITS(CRID_BODYGUARD),
         .weight    = { [MSR_RACE_DWARF]=3,    [MSR_RACE_ELF]=0,    [MSR_RACE_HALFLING]=0,    [MSR_RACE_HUMAN]=2, },
         .available = { [MSR_RACE_DWARF]=true, [MSR_RACE_ELF]=true, [MSR_RACE_HALFLING]=true, [MSR_RACE_HUMAN]=true, },
