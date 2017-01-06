89de04e280ce4d223b99da23037a0e2e7da84de5
Cor Peters <cpeters@victronenergy.com>
2017-01-06 16:13:14 +0100

    added random player

--- src/player.c
+++ src/player.c
@@ -209,6 +209,15 @@ static bool plr_action_loop(struct msr_monster *player) {
 
     lg_debug("plr_action_loop");
     while (gbl_game->running && (has_action == false) ) {
+        if (options.rnd_auto_play) {
+            int max = 100 - inp_log_key_count(gbl_game->input);
+            for (int i = max; i > 0; i--) {
+                enum inp_keys k = random_int32(gbl_game->random) % INP_KEY_MAX;
+
+                if (k == INP_KEY_QUIT) continue;
+                inp_add_to_log(gbl_game->input, k);
+            }
+        }
 
         /* Update screen dimensions if needed*/
         int lines, cols;
