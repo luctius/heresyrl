7a7eba7d765a7c8225f82df560a80a0bcacae355
Cor <prive@corpeters.nl>
2017-01-09 07:10:19 +0100

    should properly use arrays now

--- src/random_generator.c
+++ src/random_generator.c
@@ -9,16 +9,17 @@ int32_t random_gen_spawn(struct random_gen_settings *s) {
     if (s->weight == NULL) return -1;
     if (s->start_idx >= s->end_idx) return -1;
 
+    int32_t size = s->end_idx - s->start_idx;
     int32_t roll = RANDOM_GEN_MAX;
-    int32_t cumm_prob_arr[s->end_idx - s->start_idx];
+    int32_t cumm_prob_arr[size];
     int32_t sum = 0;
     int32_t idx = -1;
 
     int32_t w;
-    for (int32_t i = s->start_idx; i < s->end_idx; i++) {
+    for (int32_t i = 0; i < size; i++) {
         cumm_prob_arr[i] = RANDOM_GEN_WEIGHT_IGNORE;
 
-        w = s->weight(s->ctx, i);
+        w = s->weight(s->ctx, i + s->start_idx);
         if (w != RANDOM_GEN_WEIGHT_IGNORE) {
             sum += w;
             cumm_prob_arr[i] = sum;
@@ -28,9 +29,9 @@ int32_t random_gen_spawn(struct random_gen_settings *s) {
     if (sum == 0) return -1;
 
     roll = s->roll % sum;
-    for (int32_t i = s->end_idx; i > s->start_idx; i--) {
+    for (int32_t i = size-1; i > 0; i--) {
         if (cumm_prob_arr[i] != RANDOM_GEN_WEIGHT_IGNORE) {
-            if (roll < cumm_prob_arr[i]) idx = i;
+            if (roll < cumm_prob_arr[i]) idx = i + s->start_idx;
         }
     }
 
